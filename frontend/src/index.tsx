471b965837ef5e6506673950d7091c1c7fcb2010
prithishsamantae <prithishsamanta@gmail.com>
2025-08-24 19:13:53 -0400

    Added Analysis Page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './utils/reportWebVitals';
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
